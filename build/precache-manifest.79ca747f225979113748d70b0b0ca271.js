5293b1827c1c7f44a97a7a2aaf1e914425799a87
ickang <zpblue28@gmail.com>
2024-07-23 16:08:05 +0900

    make build files commit

---
+++ build/precache-manifest.79ca747f225979113748d70b0b0ca271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98113698ca6da392f76c589d55f26fd",
+    "url": "/react01/index.html"
+  },
+  {
+    "revision": "2f31920a7624aa41e1e6",
+    "url": "/react01/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d7a6feaacc5e8fa06eea",
+    "url": "/react01/static/js/2.e54dff62.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/react01/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "2f31920a7624aa41e1e6",
+    "url": "/react01/static/js/main.b18b91bb.chunk.js"
+  },
+  {
+    "revision": "df4ec3947c1f0107e051",
+    "url": "/react01/static/js/runtime~main.ffcf5ccf.js"
+  }
+]);
