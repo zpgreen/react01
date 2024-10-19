00decc7d7f2ade15670e03c75334e834b2c1fe99
zpgreen <zpgreen@naver.com>
2024-10-19 14:05:56 +0900

    Gang

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}>
 
       <Wrapper>
-        <Hello name="chul" firstNm="Gang" color="red" isSpeacial={true} />
+        <Hello name="chul" firstNm="KKang" color="red" isSpeacial={true} />
         <Counter />
         <InputSample></InputSample>
       </Wrapper>
